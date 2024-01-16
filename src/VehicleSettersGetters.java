a781284cffe840d433f4472877ed3f21346d9ff7
unknown <filipdjurdjevac12356@gmail.com>
2024-01-16 11:15:41 +0100

    first commit

---
+++ src/VehicleSettersGetters.java
@@ -0,0 +1,19 @@
+public class VehicleSettersGetters {
+    private String Blue, Red;
+
+    public String getBlue() {
+        return Blue;
+    }
+
+    public void setBlue(String blue) {
+        Blue = blue;
+    }
+
+    public String getRed() {
+        return Red;
+    }
+
+    public void setRed(String red) {
+        Red = red;
+    }
+}
